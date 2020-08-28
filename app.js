948ad07ad73315a2299704d8a22eb4399e16b89f
Hanzala Saeed <hanzalasaeed47@gmail.com>
2020-08-28 17:15:02 +0500

    sent data from client to the server and stored in array. (success)

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+var posts = [];
 
 const homeStartingContent =
   "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -20,19 +21,28 @@ app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.render("home", { homeParagraph: homeStartingContent });
+  console.log(posts);
 });
+
 app.get("/about", (req, res) => {
   res.render("about", { aboutParagraph: aboutContent });
 });
+
 app.get("/contact", (req, res) => {
   res.render("contact", { contactParagraph: contactContent });
 });
+
 app.get("/compose", (req, res) => {
   res.render("compose");
 });
+
 app.post("/compose", (req, res) => {
-  let compInput = req.body.comp;
-  console.log(compInput);
+  const composeData = {
+    titleInput: req.body.titleInp,
+    postInput: req.body.postBody,
+  };
+  posts.push(composeData);
+  res.redirect("/");
 });
 
 app.listen(3000, function () {
